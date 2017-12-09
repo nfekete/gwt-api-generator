860e119f58eb82e13166acad2d6ce2fb4ecef2f2
Nandor Elod Fekete <nandor.fekete@gmail.com>
2017-12-09 21:31:02 +0100

    use JsArray from latest elemental2

--- lib/com/vaadin/polymer/Polymer.java
+++ lib/com/vaadin/polymer/Polymer.java
@@ -2,7 +2,7 @@ package com.vaadin.polymer;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
-import elemental2.core.Array;
+import elemental2.core.JsArray;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.DOM;
@@ -63,11 +63,11 @@ public abstract class Polymer {
 
         <T extends HTMLElement> T querySelector(String selector);
 
-        Array querySelectorAll(String selector);
+        JsArray querySelectorAll(String selector);
 
         void appendChild(Object el);
 
-        Array children();
+        JsArray children();
     }
 
     @JsType(isNative=true, namespace=GLOBAL)
@@ -532,7 +532,7 @@ public abstract class Polymer {
      * penalty because we directly take the native array of the super ArrayList
      * implementation.
      */
-    public static native <T extends JavaScriptObject> Array<T> asJsArray(List<?> l)
+    public static native <T extends JavaScriptObject> JsArray<T> asJsArray(List<?> l)
     /*-{
         return l.@java.util.ArrayList::array;
     }-*/;
